c9e342316dfa20db00f4adaf5628caa8e9891dc5
Sébastien Braun <sebbraun@gmail.com>
2023-03-28 16:08:50 +0200

    sensitive output required

--- main.tf
+++ main.tf
@@ -23,6 +23,7 @@ output "client_certificate" {
 
 output "client_key" {
   value = "${google_container_cluster.primary.master_auth.0.client_key}"
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
